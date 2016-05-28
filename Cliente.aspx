de8702315f9bcd43046c57c4b0a98ff6847e0c94
Claudio Miranda <Claudio Miranda@INF_CLAUDIO>
2016-05-28 10:40:36 -0400

    recibo cambio en notebook seide

--- Cliente.aspx
+++ Cliente.aspx
@@ -1,5 +1,4 @@
 ﻿<%@ Page Language="VB" MasterPageFile="~/Maestro.master" AutoEventWireup="false" CodeFile="Cliente.aspx.vb" Inherits="MenuPrincipal" %>
-<%@ MasterType virtualpath="~/Maestro.master" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
       <link href="css/EstilosShop.css" rel="stylesheet" />
--- Cliente.aspx.vb
+++ Cliente.aspx.vb
@@ -5,8 +5,4 @@ Partial Class MenuPrincipal
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
 
     End Sub
-    Protected Sub BTN_Buscar_Click(sender As Object, e As EventArgs) Handles BTN_Buscar.Click
-        'Me.TXT_RutCliente.Text = Master.PropertyMasterTextBox2
-        Master.PropertyMasterTextBox2.Text = Me.TXT_RutCliente.Text
-    End Sub
 End Class
