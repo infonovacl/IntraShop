3a6a0d3299010faaa13867b15c032725ff5db567
https://github.com/infonovacl/IntraShop.git <informatica@infonova.cl>
2017-05-16 16:08:41 -0400

    PEP dia 2

--- App_Code/Class1.vb
+++ App_Code/Class1.vb
@@ -1,5 +1,6 @@
 ﻿Imports Microsoft.VisualBasic
 Public Class Globales
-    Public Shared connSTR As String = "dsn=dbdesa;uid=informix;pwd=informixdesa"
+    'Public Shared connSTR As String = "dsn=dbdesa;uid=informix;pwd=informixdesa"
+    Public Shared connSTR As String = "dsn=dbdesaweb;uid=desaweb;pwd=Dsa.web"
     Public Shared conn As System.Data.Odbc.OdbcConnection = New System.Data.Odbc.OdbcConnection(connSTR)
 End Class
