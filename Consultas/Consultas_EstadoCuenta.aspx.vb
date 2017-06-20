a428836ea87e8c9580acd967b3d4727048241af7
infonovacl <informatica@infonova.cl>
2017-06-20 01:13:18 -0400

    EECC

--- Consultas/Consultas_EstadoCuenta.aspx.vb
+++ Consultas/Consultas_EstadoCuenta.aspx.vb
@@ -73,6 +73,10 @@ Partial Class Consultas_GestionCobranza
         Next
         PDFNewDoc.Save(HttpContext.Current.Server.MapPath("~/Doc/EECC/eecc_" & Session("rut") & "_" & Session("dv") & "_" & DDL_Facturaciones.SelectedItem.Text & ".pdf"))
 
+        Dim FormatoDerecha As New XStringFormat
+        FormatoDerecha.Alignment = XStringAlignment.Far
+        FormatoDerecha.LineAlignment = XLineAlignment.Center
+
         Dim ruta_pdf_cliente As String = HttpContext.Current.Server.MapPath("~/Doc/EECC/eecc_" & Session("rut") & "_" & Session("dv") & "_" & DDL_Facturaciones.SelectedItem.Text & ".pdf") ' PDF destino 
         Dim PDFDoc2 As PdfSharp.Pdf.PdfDocument = PdfSharp.Pdf.IO.PdfReader.Open(ruta_pdf_cliente, PdfDocumentOpenMode.Modify)
         Dim pp As PdfSharp.Pdf.PdfPage = PDFDoc2.Pages(0) '= PDFNewDoc.AddPage(PDFDoc.Pages(0))
@@ -89,16 +93,53 @@ Partial Class Consultas_GestionCobranza
                 Dim fecha_pago As String = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(12, 8)
                 ConvierteStringAFecha(fecha_pago)
                 gfx.DrawString(Trim(fecha_pago), font, XBrushes.Black, New XVector(180, 75))
-                Me.LBL_FacturacionesError.Visible = True
-                LBL_FacturacionesError.Text = nombres
+                Dim cupo_aprobado As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(228, 9)
+                Dim cupo_utilizado As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(237, 9)
+                Dim cupo_disponible As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(246, 9)
+                gfx.DrawString(Trim(Format(cupo_aprobado, "###,#0")), font, XBrushes.Black, New XRect(105, 108, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(cupo_utilizado, "###,#0")), font, XBrushes.Black, New XRect(195, 108, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(cupo_disponible, "###,#0")), font, XBrushes.Black, New XRect(285, 108, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                Dim cupo_aprobado_avance As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(255, 9)
+                Dim cupo_utilizado_avance As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(264, 9)
+                Dim cupo_disponible_avance As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(273, 9)
+                gfx.DrawString(Trim(Format(cupo_aprobado_avance, "###,#0")), font, XBrushes.Black, New XRect(105, 120, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(cupo_utilizado_avance, "###,#0")), font, XBrushes.Black, New XRect(195, 120, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(cupo_disponible_avance, "###,#0")), font, XBrushes.Black, New XRect(285, 120, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                Dim tasa_interes_cuotas As Decimal = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(300, 9)
+                Dim tasa_interes_avance As Decimal = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(309, 9)
+                gfx.DrawString(Trim(Format(tasa_interes_cuotas, "###,##0") & "%"), font, XBrushes.Black, New XRect(115, 153, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(tasa_interes_avance, "###,##0") & "%"), font, XBrushes.Black, New XRect(245, 153, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                Dim cae_cuotas As Decimal = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(318, 9)
+                Dim cae_avance As Decimal = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(327, 9)
+                gfx.DrawString(Trim(Format(cae_cuotas, "###,##0") & "%"), font, XBrushes.Black, New XRect(115, 165, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(cae_avance, "###,##0") & "%"), font, XBrushes.Black, New XRect(245, 165, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                Dim fecha_fact_desde As String = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(336, 8)
+                ConvierteStringAFecha(fecha_fact_desde)
+                gfx.DrawString(Trim(fecha_fact_desde), font, XBrushes.Black, New XVector(475, 162))
+                Dim fecha_fact_hasta As String = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(344, 8)
+                ConvierteStringAFecha(fecha_fact_hasta)
+                gfx.DrawString(Trim(fecha_fact_hasta), font, XBrushes.Black, New XVector(530, 162))
+                Dim fecha_fact_desde_ant As String = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(352, 8)
+                ConvierteStringAFecha(fecha_fact_desde_ant)
+                gfx.DrawString(Trim(fecha_fact_desde_ant), font, XBrushes.Black, New XVector(170, 223))
+                Dim fecha_fact_hasta_ant As String = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(360, 8)
+                ConvierteStringAFecha(fecha_fact_hasta_ant)
+                gfx.DrawString(Trim(fecha_fact_hasta_ant), font, XBrushes.Black, New XVector(290, 223))
+
+                Dim saldo_adeudado_periodo_ant As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(368, 9)
+                Dim monto_facturado_periodo_ant As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(377, 9)
+                Dim monto_pagado_periodo_ant As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(386, 9)
+                Dim saldo_adeudado_final_periodo_ant As Integer = Me.Grilla_TramaEECC.Rows(0).Cells(0).Text.Substring(395, 9)
+                gfx.DrawString(Trim(Format(saldo_adeudado_periodo_ant, "###,#0")), font, XBrushes.Black, New XRect(285, 232, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(monto_facturado_periodo_ant, "###,#0")), font, XBrushes.Black, New XRect(285, 244, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(monto_pagado_periodo_ant, "###,#0")), font, XBrushes.Black, New XRect(285, 257, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(saldo_adeudado_final_periodo_ant, "###,#0")), font, XBrushes.Black, New XRect(285, 269, 80, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
             End If
         Next
-        Dim FormatoDerecha As New XStringFormat
-        FormatoDerecha.Alignment = XStringAlignment.Far
-        FormatoDerecha.LineAlignment = XLineAlignment.Center
 
-        Dim RECT_D3_Monto_Operacion As New XRect(313, 328, 40, 10) 'izquierda,abajo,ancho,alto
-        gfx.DrawRectangle(XBrushes.SeaShell, RECT_D3_Monto_Operacion)
+
+        ' Dim RECT_D3_Monto_Operacion As New XRect(173, 140, 80, 10) 'izquierda,abajo,ancho,alto
+        ' gfx.DrawRectangle(XBrushes.SeaShell, RECT_D3_Monto_Operacion)
 
         Dim L_D1 As Integer = 0
         For L = 0 To Grilla_TramaEECC.Rows.Count - 1
@@ -113,14 +154,13 @@ Partial Class Consultas_GestionCobranza
                 Dim total_cuotas As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(133, 3)
                 Dim monto_cuota As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(136, 9)
 
-                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 328 + L_D1))
-                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 328 + L_D1))
-                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 328 + L_D1))
-                'gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XVector(325, 328 + L_D1))
-                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 335 + L_D1, 74, 10), FormatoDerecha)
-                gfx.DrawString(Trim(Format(monto_total_a_pagar, "###,#0")), font, XBrushes.Black, New XVector(365, 328 + L_D1))
-                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 335 + L_D1))
-                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XVector(530, 335 + L_D1))
+                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 321 + L_D1))
+                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 321 + L_D1))
+                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 321 + L_D1))
+                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 313 + L_D1, 40, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(monto_total_a_pagar, "###,#0")), font, XBrushes.Black, New XRect(355, 313 + L_D1, 62, 10), FormatoDerecha)
+                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 321 + L_D1))
+                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XRect(505, 313 + L_D1, 75, 10), FormatoDerecha)
                 L_D1 = L_D1 + 8 ' SIGUIENTE LINEA
             End If
         Next
@@ -138,14 +178,13 @@ Partial Class Consultas_GestionCobranza
                 Dim total_cuotas As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(133, 3)
                 Dim monto_cuota As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(136, 9)
 
-                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 450 + L_D2))
-                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 450 + L_D2))
-                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 450 + L_D2))
-                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 443 + L_D2, 40, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
-                gfx.DrawString(Trim(Format(monto_total_a_pagar, "###,#0")), font, XBrushes.Black, New XRect(355, 443 + L_D2, 62, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
-                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 450 + L_D2))
-                ' gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XVector(530, 450 + L_D2))
-                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XRect(505, 443 + L_D2, 74, 10), FormatoDerecha)
+                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 454 + L_D2))
+                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 454 + L_D2))
+                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 454 + L_D2))
+                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 446 + L_D2, 40, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(monto_total_a_pagar, "###,#0")), font, XBrushes.Black, New XRect(355, 446 + L_D2, 62, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 454 + L_D2))
+                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XRect(505, 446 + L_D2, 74, 10), FormatoDerecha)
                 L_D2 = L_D2 + 8
             End If
         Next
@@ -163,13 +202,13 @@ Partial Class Consultas_GestionCobranza
                 Dim total_cuotas As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(133, 3)
                 Dim monto_cuota As Integer = Me.Grilla_TramaEECC.Rows(L).Cells(0).Text.Substring(136, 9)
 
-                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 511 + L_D3))
-                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 511 + L_D3))
-                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 511 + L_D3))
-                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 504 + L_D3, 40, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
-                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(355, 504 + L_D3, 67, 10), FormatoDerecha)
-                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 511 + L_D3))
-                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XRect(505, 504 + L_D3, 75, 10), FormatoDerecha)
+                gfx.DrawString(Trim(sucursal), font, XBrushes.Black, New XVector(32, 516 + L_D3))
+                gfx.DrawString(Trim(fecha_trans), font, XBrushes.Black, New XVector(115, 516 + L_D3))
+                gfx.DrawString(Trim(descripcion), font, XBrushes.Black, New XVector(173, 516 + L_D3))
+                gfx.DrawString(Trim(Format(monto_operacion, "###,#0")), font, XBrushes.Black, New XRect(313, 508 + L_D3, 40, 10), FormatoDerecha) 'izquierda,abajo,ancho,alto
+                gfx.DrawString(Trim(Format(monto_total_a_pagar, "###,#0")), font, XBrushes.Black, New XRect(355, 508 + L_D3, 62, 10), FormatoDerecha)
+                gfx.DrawString(Trim(numero_cuota & " / " & total_cuotas), font, XBrushes.Black, New XVector(450, 516 + L_D3))
+                gfx.DrawString(Trim(Format(monto_cuota, "###,#0")), font, XBrushes.Black, New XRect(505, 508 + L_D3, 75, 10), FormatoDerecha)
                 L_D3 = L_D3 + 8
             End If
         Next
